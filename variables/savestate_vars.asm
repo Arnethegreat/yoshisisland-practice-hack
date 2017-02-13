00b3607247b32ba7e3739936c32d3655f97bd74a
Tim Alakangas <Arnethegreat@gmail.com>
2017-02-13 15:17:56 +0100

    save more stuff

--- variables/savestate_vars.asm
+++ variables/savestate_vars.asm
@@ -33,38 +33,39 @@
 !sram_block_00_savestate = $7F0300
 !sram_block_00_size = #$2200
 
-; $2600 -> $449E 
+; $2600 -> $409E 
 !sram_block_01_source = $702600
 !sram_block_01_savestate = $7F2500
 !sram_block_01_size = #$1A9E
 
-; Not used
-!sram_block_02_source = $700B0F
-!sram_block_02_savestate = $7F439E
-!sram_block_02_size = #$0000
+; $49C6 -> $49E8
+; sluggy/froggy wall stuff
+!sram_block_02_source = $7049C6
+!sram_block_02_savestate = $7F3F9E
+!sram_block_02_size = #$0022
 
-; Not used
-!sram_block_03_source = $70409E
-!sram_block_03_savestate = $7F3E96
-!sram_block_03_savestate = $7F3E96
-!sram_block_03_size = #$0000
+; $4C76 -> $4CB4
+; sluggy info
+!sram_block_03_source = $704C76
+!sram_block_03_savestate = $7F3FC0
+!sram_block_03_size = #$003E
 
 ; WRAM blocks
 ;
 
-; $0306 -> $03BE
-!wram_block_00_source = $7E0306
-!wram_block_00_savestate = $7F0000
-!wram_block_00_size = #$01BE
+; $0300 -> $03C0
+!wram_block_00_source = $7E0300
+!wram_block_00_savestate = $7F3FFE
+!wram_block_00_size = #$00C0
 
 ; $0B0F -> $11B7 
 !wram_block_01_source = $7E0B0F
-!wram_block_01_savestate = $7F439E
+!wram_block_01_savestate = $7F40BE
 !wram_block_01_size = #$06A8
 
 ; Register mirrors test
 !wram_block_02_source = $7E094A
-!wram_block_02_savestate = $7F4A46
+!wram_block_02_savestate = $7F4766
 !wram_block_02_size = #$0024
 
 ; Not used

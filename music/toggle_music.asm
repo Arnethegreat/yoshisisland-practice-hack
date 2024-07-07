c4b78dc307bd68c58d8be240bd19c9b8ec1bec52
cbrgray <cbrgray@gmail.com>
2024-07-07 20:15:20 +0100

    don't restart music on exiting menu

--- music/toggle_music.asm
+++ music/toggle_music.asm
@@ -5,6 +5,7 @@ toggle_music:
     BNE .disable_music
 
 .set_music
+    LDA !reg_apu_port0 : BNE .ret ; if music is already playing, don't restart it
     LDA #$01
     STA !r_apu_io_0_mirror
     BRA .ret
